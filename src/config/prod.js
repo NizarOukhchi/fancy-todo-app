5826e7944d4624f0a8300e1c9ffeb6ea3f8050ef
Nizar Oukhchi <nizar.oukhchi@gmail.com>
2018-06-25 09:02:52 +0100

    add firebase testing key

---
+++ src/config/prod.js
@@ -0,0 +1,9 @@
+export const FirebaseConfig = {
+    apiKey: 'AIzaSyCNfqfJrPVtqxo67KvEn63cOXDNbMu82YQ',
+    authDomain: 'fancy-tasks-manager.firebaseapp.com',
+    databaseURL: 'https://fancy-tasks-manager.firebaseio.com',
+    projectId: 'fancy-tasks-manager',
+    storageBucket: '',
+    messagingSenderId: '469903459988'
+  };
+  
